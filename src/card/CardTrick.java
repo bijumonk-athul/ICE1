3a6def5640d154776c080db78afea5bb44a6b2b2
bijumonk-athul <bijumonk@sheridancollege.ca>
2025-01-21 19:09:39 -0500

    master copy

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -21,7 +21,37 @@ public class CardTrick {
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) (Math.random() * 13) + 1);
+            c.setSuit(Card.SUITS[(int) (Math.random() * 4)]); 
+            magicHand[i] = c;
+            System.out.println(c.getValue() + " of " + c.getSuit());
         }
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a Value from 1-13 :");
+        int V1 = sc.nextInt();
+        System.out.print("Enter a Suit (0-3 where 0=Hearts,1=Diamond,2=Spades,3=Clubs): ");
+        int suitIndex = sc.nextInt();
+        String S1 = Card.SUITS[suitIndex]; 
+        
+        Card c1 = new Card();
+        c1.setValue(V1);
+        c1.setSuit(S1);
+        
+        boolean usercard = false;
+        for (Card C : magicHand) {
+            if (C.getValue() == c1.getValue() && C.getSuit().equals(c1.getSuit())) {
+                usercard = true;
+                break;
+            }
+        }
+         if (usercard==true){
+             System.out.println("Your Assumption is Correct");
+         }
+         else{
+             System.out.println("Sorry,Your Assumption is Incorrect");
+         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
